67eca23ae6da5886702db037b9fd7fb56645cbe0
Fatur Hanafi <faturh71@gmail.com>
2026-02-23 16:09:35 +0700

    Update calc.py

---
+++ calculator/calc/calc.py
@@ -0,0 +1,16 @@
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if b == 0:
+        raise ValueError("Tidak bisa membagi dengan nol")
+    return a / b
+
+def power(a, b):
+    return a ** b
