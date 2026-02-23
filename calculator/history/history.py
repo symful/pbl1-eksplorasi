6d154f91bb0be920d058c3d46541af0d3db6c778
HAFIZ FAUZAN SYAFRUDIN <hafiz.fauzan.tif425@polban.ac.id>
2026-02-23 23:59:08 +0700

    Implementasi memori kalkulator

--- calculator/history/history.py
+++ calculator/history/history.py
@@ -1,4 +1,5 @@
 _history = []
+_memory = 0.0
 
 def add_entry(expression, result):
     """Menambahkan entri baru ke riwayat perhitungan."""
@@ -11,3 +12,19 @@ def get_history():
 def clear_history():
     """Menghapus semua data dalam riwayat perhitungan."""
     _history.clear()
+
+def memory_add(value):
+    """Menambahkan nilai ke memori kalkulator (M+)."""
+    global _memory
+    _memory += float(value)
+def memory_sub(value):
+    """Mengurangi nilai dari memori kalkulator (M-)."""
+    global _memory
+    _memory -= float(value)
+def memory_recall():
+    """Mengambil nilai yang tersimpan di memori (MR)."""
+    return _memory
+def memory_clear():
+    """Menghapus/mereset nilai memori menjadi 0.0 (MC)."""
+    global _memory
+    _memory = 0.0
