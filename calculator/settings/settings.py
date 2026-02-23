b72e60b5585d24f23b1fabb0c0f1df25c605a6dd
ExuNova <exufrostnova@gmail.com>
2026-02-23 22:04:08 +0700

    Add light theme
    - Add light theme in settings.py

--- calculator/settings/settings.py
+++ calculator/settings/settings.py
@@ -17,6 +17,22 @@ THEMES = {
         "btn_clear_bg": "#f38ba8",
         "btn_clear_fg": "#1e1e2e",
     },
+    "light": {
+        "bg": "#eff1f5",
+        "fg": "#4c4f69",
+        "display_bg": "#dce0e8",
+        "display_fg": "#4c4f69",
+        "btn_bg": "#ccd0da",
+        "btn_fg": "#4c4f69",
+        "btn_op_bg": "#1e66f5",
+        "btn_op_fg": "#eff1f5",
+        "btn_eq_bg": "#40a02b",
+        "btn_eq_fg": "#eff1f5",
+        "btn_func_bg": "#04a5e5",
+        "btn_func_fg": "#eff1f5",
+        "btn_clear_bg": "#d20f39",
+        "btn_clear_fg": "#eff1f5",
+    },
 }
 
 
