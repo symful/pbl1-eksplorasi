40c17e94fee9ed722687a7abec179721ffb2d3d5
Fatur Hanafi <faturh71@gmail.com>
2026-03-06 16:44:38 +0700

    Add threading

---
+++ ui/threading.py
@@ -0,0 +1,23 @@
+from PyQt5.QtCore import QThread, pyqtSignal
+from scrapers.kompas import KompasScraper
+from scrapers.detik import DetikScraper
+from utils.saver import DataSaver
+
+class ScrapeWorker(QThread):
+    finished = pyqtSignal(list)
+    error = pyqtSignal(str)
+
+    def __init__(self, portal, count):
+        super().__init__()
+        self.portal = portal
+        self.count = count
+
+    def run(self):
+        try:
+            scraper = KompasScraper() if self.portal == "Kompas" else DetikScraper()
+            results = scraper.scrape(self.count)
+            DataSaver.save_to_csv(results)
+            DataSaver.save_to_json(results)
+            self.finished.emit(results)
+        except Exception as e:
+            self.error.emit(str(e))
